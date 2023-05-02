f49f767444bfe20042bfc151c92dc005ea28eca2
wawawa <1457480465@qq.com>
2023-05-02 11:38:33 +0800

    2023/5/2 leetcode hot100 二叉树的中序遍历

---
+++ leetcode/LC_94.go
@@ -0,0 +1,23 @@
+package main
+
+/*
+*
+94. 二叉树的中序遍历
+*/
+func inorderTraversal(root *TreeNode) (ans []int) {
+	var dfs func(*TreeNode)
+	dfs = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		dfs(root.Left)
+		ans = append(ans, root.Val)
+		dfs(root.Right)
+	}
+	dfs(root)
+	return ans
+}
+
+func main() {
+
+}
