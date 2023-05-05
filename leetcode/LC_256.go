4c2ffb9a185b5fb67d047092395b8cb0155ebd8a
wawawa <1457480465@qq.com>
2023-05-05 18:13:07 +0800

    2023/5/5 leetcode 256. 粉刷房子(Plus会员)

---
+++ leetcode/LC_256.go
@@ -0,0 +1,26 @@
+package main
+
+/*
+*
+256. 粉刷房子
+*/
+//func minCost(costs [][]int) int {
+//	n := len(costs)
+//	f := make([][3]int, 110)
+//	for i, x := range costs {
+//		f[i+1][0] = min(f[i][1], f[i][2]) + x[0]
+//		f[i+1][1] = min(f[i][0], f[i][2]) + x[1]
+//		f[i+1][2] = min(f[i][0], f[i][1]) + x[2]
+//	}
+//	return min(f[n][0], min(f[n][1], f[n][2]))
+//}
+//
+//func min(a, b int) int {
+//	if a < b {
+//		return a
+//	}
+//	return b
+//}
+
+func main() {
+}
