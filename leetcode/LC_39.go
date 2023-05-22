a1b3fa90dc3dab92a26316c97990bdd22519f8b2
wawawa <1457480465@qq.com>
2023-05-22 19:07:44 +0800

    2023/5/22 leetcode hot100

---
+++ leetcode/LC_39.go
@@ -0,0 +1,24 @@
+package main
+
+//39. 组合总和
+
+func combinationSum(candidates []int, target int) (ans [][]int) {
+	comb := []int{}
+	var dfs func(int, int)
+	dfs = func(idx int, target int) {
+		if target == 0 {
+			ans = append(ans, append([]int(nil), comb...))
+			return
+		}
+		if target < 0 {
+			return
+		}
+		for i := idx; i < len(candidates); i++ {
+			comb = append(comb, candidates[i])
+			dfs(i, target-candidates[i])
+			comb = comb[:len(comb)-1]
+		}
+	}
+	dfs(0, target)
+	return ans
+}
---
+++ leetcode/LC_739.go
@@ -0,0 +1,19 @@
+package main
+
+//739. 每日温度
+
+func dailyTemperatures(t []int) []int {
+	n := len(t)
+	st := []int{}
+	ans := make([]int, n)
+	for i := n - 1; i >= 0; i-- {
+		for len(st) > 0 && t[st[0]] <= t[i] {
+			st = st[1:]
+		}
+		if len(st) > 0 {
+			ans[i] = st[0] - i
+		}
+		st = append([]int{i}, st...)
+	}
+	return ans
+}
